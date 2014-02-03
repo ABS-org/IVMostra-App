ab4cf3e42d4a86f65bc5e3574e905c88f30d527a
Feel Fine <danielfeelfine@gmail.com>
2014-02-03 12:25:12 -0300

    Update Gruntfile.js
    with grunt-bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,33 @@ module.exports = function (grunt) {
     // Metadata
     pkg: grunt.file.readJSON('package.json'),
 
+    /************************************
+     * grunt-bump
+     * Bump package version, create tag, commit, push...
+     ************************************/
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'v%VERSION%',
+        commitFiles: ['package.json'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'v%VERSION%',
+        push: true,
+        pushTo: 'master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+      }
+    }
+
   });
 
 
   // Load multiple grunt tasks using globbing patterns
   require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+  // Use grunt-bump for changing version number
+  grunt.loadNpmTasks('grunt-bump');
 
 };
