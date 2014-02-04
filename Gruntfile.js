87a9a8a95ae2583fc08ad434c6d97b37577b7e97
Feel Fine <danielfeelfine@gmail.com>
2014-02-04 10:03:17 -0300

    Add iscroll
    in bower and in concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,7 @@ module.exports = function (grunt) {
           // Libraries
           'www/bower_components/zeptojs/src/zepto.js',
           'www/bower_components/fastclick/lib/fastclick.js',
+          'www/bower_components/iscroll/build/iscroll.js',
           'www/bower_components/hammerjs/hammer.min.js',
 
           // App
