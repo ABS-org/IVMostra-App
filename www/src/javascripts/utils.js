e5c268d12aef9bc1c3be636d5deabaee76e74957
Feel Fine <danielfeelfine@gmail.com>
2014-02-15 20:54:55 -0300

    Init utils.js

---
+++ www/src/javascripts/utils.js
@@ -0,0 +1,20 @@
+$("#social").bind('pageinit', function() {
+
+        /*
+    if(localStorage.hasItem("facebook_accessToken")){
+        alert('ok')
+        var token = window.localStorage.getItem('facebook_accessToken');
+
+        if(token === null){
+    
+            $('#facebook_login').click(acebook.onFacebookLogin);
+            
+        }else{
+            
+            $('#facebook_login').click(facebook.onFacebookLogout);
+            
+        }
+        */
+    }
+    
+});
