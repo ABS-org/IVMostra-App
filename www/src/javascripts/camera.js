ad5c30c24f1533b9a37fd2e105605caa63a2da3a
Feel Fine <danielfeelfine@gmail.com>
2014-02-15 20:53:38 -0300

    Update camera.js
    Add upload_foto var

--- www/src/javascripts/camera.js
+++ www/src/javascripts/camera.js
@@ -1,6 +1,6 @@
-
-    var pictureSource;   // picture source
+var pictureSource;   // picture source
     var destinationType; // sets the format of returned value
+    var upload_foto;
 
     // Wait for device API libraries to load
     //
@@ -16,11 +16,6 @@
     // Called when a photo is successfully retrieved
     //
     function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64-encoded image data
-      // console.log(imageData);
-
-      // Get image handle
-      //
       var smallImage = document.getElementById('smallImage');
 
       // Unhide image elements
@@ -31,52 +26,29 @@
       // The in-line CSS rules are used to resize the image
       //
       smallImage.src = "data:image/jpeg;base64," + imageData;
+
+      $(".btnCompartilhar").hide();
+      $(".loading-upload").show();
+      $.post('http://sandbox.cachina.com.br/upload.php',{imagem: imageData},function(data){
+          //alert(data)
+         upload_foto = data;
+         $(".btnCompartilhar").show();
+         $(".loading-upload").hide();
+      })
     }
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI
-      // console.log(imageURI);
+  function capturePhotoEdit() {
 
-      // Get image handle
-      //
-      var largeImage = document.getElementById('smallImage');
+      if(window.localStorage.getItem('facebook_accessToken') === "null" ||window.localStorage.getItem('facebook_accessToken') === "") {
+          alert("Realize o Login no Facebook!");
+          return false;
+      }
 
-      // Unhide image elements
-      //
-      largeImage.style.display = 'block';
-
-      // Show the captured photo
-      // The in-line CSS rules are used to resize the image
-      //
-      largeImage.src = imageURI;
-    }
-
-    // A button will call this function
-    //
-    function capturePhoto() {
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    // A button will call this function
-    //
-    function capturePhotoEdit() {
       // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
         destinationType: destinationType.DATA_URL });
     }
 
-    // A button will call this function
-    //
-    function getPhoto(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
 
     // Called if something bad happens.
     //
