fdb6460af979406860d2ca10b285226ac698c025
Daniel Torres <danielfeelfine@gmail.com>
2014-02-17 02:17:22 -0300

    Update utils.js
    Put index inline js in this

--- www/src/javascripts/utils.js
+++ www/src/javascripts/utils.js
@@ -1,20 +1,41 @@
-$("#social").bind('pageinit', function() {
 
-        /*
-    if(localStorage.hasItem("facebook_accessToken")){
-        alert('ok')
-        var token = window.localStorage.getItem('facebook_accessToken');
+function postFace(face,foto) {
 
-        if(token === null){
-    
-            $('#facebook_login').click(acebook.onFacebookLogin);
-            
-        }else{
-            
-            $('#facebook_login').click(facebook.onFacebookLogout);
-            
-        }
-        */
-    }
-    
+  if(!face.isFacebookLogged()){
+    alert("Realize o Login no Facebook");
+    return false;
+  }
+ 
+  var photo = foto;
+  
+  var a = {message: 'Olá! Estou na IV Mostra Nacional de Experiências em Atenção Básica/Saúde da Família. #IVMostra #MostraSaúde',
+  link: 'http://www.mostrasaude.net/',
+  picture: foto,
+  name: 'IV Mostra',
+  caption: 'http://www.mostrasaude.net/',
+  description: 'Valorizando a experiência, estimulando o protagonismo local.'};
+
+  face.onFacebookPostFeed(a);
+  face.onFacebookLogout();
+  
+  var smallImage = document.getElementById('smallImage');
+  smallImage.src = "data:image/jpeg;base64,";
+
+  alert('Compartilhado com Sucesso! ;-)')
+
+}
+
+
+$("#social").bind('pageshow', function() {
+
+  window.localStorage.setItem('facebook_accessToken', null);
+  if(facebook.isFacebookLogged()){
+      $('#facebook_login').addClass("logoff");
+      $('#facebook_login').click(facebook.onFacebookLogout);
+
+  }else{ //NAO TIVER LOGADO
+      $('#facebook_login').removeClass("logoff");
+      $('#facebook_login').click(facebook.onFacebookLogin);
+  }
+
 });
